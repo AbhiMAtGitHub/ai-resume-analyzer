bbc436402b4ad868ef510dc47161204eb65940b4
Abhishek M <iamabhishek10920@gmail.com>
2025-10-24 21:29:31 +0530

    Fixed errors

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,6 +1,11 @@
 resource "aws_s3_bucket" "resume_analyzer_bucket" {
-  bucket        = var.bucket_name
+  bucket        = "ai-resume-analyzer-bucket"
   force_destroy = var.bucket_force_destroy
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = [bucket]
+  }
 
   tags = var.bucket_tags
 }
@@ -43,16 +48,4 @@ output "s3_bucket_name" {
 
 output "s3_bucket_arn" {
   value = aws_s3_bucket.resume_analyzer_bucket.arn
-}
-
-resource "aws_s3_bucket" "resume_analyzer_bucket" {
-  bucket        = "ai-resume-analyzer-bucket"
-  force_destroy = var.bucket_force_destroy
-
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes  = [bucket]
-  }
-
-  tags = var.bucket_tags
 }
