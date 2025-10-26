1a87a1919bc3471960e43a8e3acbc5abe19caf05
Abhishek M <iamabhishek10920@gmail.com>
2025-10-26 22:42:29 +0530

    Fix

--- infra/ecr_policy.tf
+++ infra/ecr_policy.tf
@@ -1,4 +1,3 @@
-# Attach repository policy to existing ECR repo (created externally)
 data "aws_ecr_repository" "file_handler_repo" {
   name = "resume-analyzer-file-handler"
 }
@@ -10,7 +9,7 @@ resource "aws_ecr_repository_policy" "file_handler_repo_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Sid = "AllowLambdaServiceToPullImages",
+        Sid = "AllowLambdaToPullImages",
         Effect = "Allow",
         Principal = {
           Service = "lambda.amazonaws.com"
@@ -19,6 +18,19 @@ resource "aws_ecr_repository_policy" "file_handler_repo_policy" {
           "ecr:GetDownloadUrlForLayer",
           "ecr:BatchGetImage",
           "ecr:BatchCheckLayerAvailability"
+        ]
+      },
+      {
+        Sid = "AllowAccountAccess",
+        Effect = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        },
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage"
         ]
       }
     ]
